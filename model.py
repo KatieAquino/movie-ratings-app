fc78bb15731d24b26d1fc857ee1d35fab3c64802
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-02-03 20:42:00 +0000

    Creates Users table

--- model.py
+++ model.py
@@ -5,8 +5,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Replace this with your code!
+class User(db.Model):
+    """A user"""
 
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincremnt=True,
+                        primary_key=True)
+    
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
