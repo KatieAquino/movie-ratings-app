0447cac9a65393f39b782f530b61329e6612b236
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-02-05 05:34:11 +0000

    Completes rating & movie objects

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 """Models for movie ratings app."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 db = SQLAlchemy()
 
@@ -11,18 +13,19 @@ class User(db.Model):
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer,
-                        autoincremnt=True,
+                        autoincrement=True,
                         primary_key=True)
     
-    email = db.Column(db.String, unique=True, nullable=False, unique=True)
+    email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         """Represents User Object"""
+
         return f'<User user_id={self.user_id} email={self.email}>'
 
-class Movie(db.model):
-    """A Movie""""
+class Movie(db.Model):
+    """A movie"""
 
     __tablename__ = "movies"
 
@@ -39,6 +42,24 @@ class Movie(db.model):
 
         return f'<Movie movie_id={self.movie_id} title={self.title} release_date={self.release_date}>'
 
+class Rating(db.Model):
+    """A Rating"""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer,
+                          primary_key=True,
+                          autoincrement=True,)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
+
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
@@ -46,12 +67,14 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
 
     db.app = flask_app
     db.init_app(flask_app)
+    db.create_all()
 
     print('Connected to the db!')
 
 
 if __name__ == '__main__':
     from server import app
+
 
     # Call connect_to_db(app, echo=False) if your program output gets
     # too annoying; this will tell SQLAlchemy not to print out every
